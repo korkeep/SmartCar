944bc2b5d1309766c7c21fed5b179817c212d1ec
KSS <korkeep@naver.com>
2020-02-13 18:24:32 +0900

    TCP Login 예외처리 구현 완료

--- SmartCar_Client/app/src/main/java/com/example/smartcar_client/LoginActivity.java
+++ SmartCar_Client/app/src/main/java/com/example/smartcar_client/LoginActivity.java
@@ -1,29 +1,28 @@
 package com.example.smartcar_client;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.nio.charset.StandardCharsets;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText et_Raspberry, et_ID, et_Password;
-    private Button btn_Login;
-
-    private Socket socket;      // TCP socket 통신
-    private int port = 10080;   // 포트번호
+    private Socket socket;
 
     //onStop 상태면 Socket 종료해준다
     @Override
@@ -47,7 +46,7 @@ public class LoginActivity extends AppCompatActivity {
         et_Raspberry = findViewById(R.id.Raspberry);
 
         //Login 기능 구현
-        btn_Login = findViewById(R.id.Login);
+        Button btn_Login = findViewById(R.id.Login);
         btn_Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,6 +60,7 @@ public class LoginActivity extends AppCompatActivity {
                     SetSocket setSocket = new SetSocket(Raspberry, ID, Password);
                     setSocket.execute();
                 } catch (Exception e) {
+                    System.out.println("**** 예외가 발생했다!!! ****");
                     e.printStackTrace();
                 }
             }
@@ -69,13 +69,12 @@ public class LoginActivity extends AppCompatActivity {
 
     //TCP 통신을 위한 Socket 함수
     @SuppressLint("StaticFieldLeak")
-    public class SetSocket extends AsyncTask<Void, Void, Void> {
+    public class SetSocket extends AsyncTask<Void, Void, Boolean> {
         //소켓 통신을 위한 변수 + 결과값 받아오는 변수 선언
         String id = "";
         String password = "";
         String raspberry = "";
         String sendBuf = "";
-        StringBuffer response = new StringBuffer();
 
         //Constructor
         SetSocket(String Rasp, String ID, String PW) {
@@ -91,56 +90,82 @@ public class LoginActivity extends AppCompatActivity {
 
         //Background TCP 연결 시도
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Boolean doInBackground(Void... params) {
             try {
                 System.out.println("★★★★  Now, we are in Async Background  ★★★★");
 
-                //Socket 연결 (Server ← Client)
-                socket = new Socket(raspberry, port);
+                //Socket 연결 (Server ← Client), port : 10080
+                socket = new Socket(raspberry, 10080);
 
                 //Login 절차 구현 → sendBuf : ID, PW, 초기 Speed 저장해서 송신
                 sendBuf = "AD" + id + '@' + password + "@login0DD";
                 OutputStream out = socket.getOutputStream();
                 out.write(sendBuf.getBytes());
+                return true;
 
-                //Raspberry(Server) Data 수신
-                /*ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
-                byte[] buffer = new byte[1024];
-                int bytesRead;
-                InputStream inputStream = socket.getInputStream();
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    byteArrayOutputStream.write(buffer, 0, bytesRead);
-                    response.append(byteArrayOutputStream.toString("UTF-8"));
-                }
-                System.out.println("    Data 수신 성공");*/
-
-                //응답이 ~~라면 로그인 성공, 화면 전환
-                //임시 화면 전환
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+                /*
+                tcp_server.c 확인 결과, return 값을 Client 쪽에 넘겨주지 않고,
+                Printf 함수를 사용해 내부 terminal 상에서 Log 통해 확인하도록 짜여있다.
+                */
 
             } catch (UnknownHostException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
-                response.append("UnknownHostException: " + e.toString());
+                System.out.println("UnknownHostException: " + e.toString());
+                return false;
             } catch (IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
-                response.append("IOException: " + e.toString());
+                System.out.println("IOException: " + e.toString());
+                return false;
             }
-            System.out.println("Server Response :   " + response);
-            return null;
         }
 
         //Background 실행 후 결과
         @Override
-        protected void onPostExecute(Void result) {
+        protected void onPostExecute(Boolean result) {
             super.onPostExecute(result);
 
-            //MainActivity 넘어감
-                /*Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);*/
+            /*
+            tcp_server.c의 ID,PW(mesl, 348)를 변경해서 컴파일 후 동작시켜 보니,
+            TCPAgent.exe 프로그램 자체에서 Login 시도를 할 수 없도록 막고 있었다.
+            TCPAgent.exe 프로그램에서도 ID,PW(kisa, 1234) 하드코딩 돼 있을 확률 ↑
+            다른 Raspberry IP 주소가 입력된 경우의 예외처리까지 성공!!!
+            */
+
+            //ID,PW(kisa, 1234) Check
+            if (!(id.equals("kisa") && password.equals("1234"))) {
+                setCustomToast(LoginActivity.this, "ID, PW가 일치하지 않습니다");
+            } else {
+                if (!result) {
+                    setCustomToast(LoginActivity.this, "TCP 통신에 문제가 발생했습니다");
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            }
         }
+    }
+
+    //Custom Toast Message
+    public static void setCustomToast(Context context, String msg) {
+        TextView m_temp = new TextView(context);
+        m_temp.setBackgroundResource(R.color.colorItem);
+        m_temp.setPadding(32, 32, 32, 32);
+        m_temp.setTextSize(16);
+        m_temp.setText(msg);
+
+        final Toast toast = Toast.makeText(context, "", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM, 0, 48);
+        toast.setView(m_temp);
+        toast.show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                toast.cancel();
+            }
+        }, 1000);
     }
 }
 
--- SmartCar_Client/app/src/main/java/com/example/smartcar_client/MainActivity.java
+++ SmartCar_Client/app/src/main/java/com/example/smartcar_client/MainActivity.java
@@ -201,5 +201,4 @@ public class MainActivity extends AppCompatActivity {
             }
         }, 1000);
     }
-
 }
